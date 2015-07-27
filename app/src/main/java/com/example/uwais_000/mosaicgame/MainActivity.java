3e5496f77afff980f09fce03bac3944ff3d477a2
Uwais Iqbal <uwais.iqbal94@gmail.com>
2015-07-27 14:56:48 +0100

    First Commit - Working version with local multiplayer.
    Still need to add code to upload game state to server.

---
+++ app/src/main/java/com/example/uwais_000/mosaicgame/MainActivity.java
@@ -0,0 +1,262 @@
+package com.example.uwais_000.mosaicgame;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.CountDownTimer;
+import android.os.SystemClock;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    int gridSize, players, turnTime, rounds;
+    TableLayout table;
+    RelativeLayout root;
+    RelativeLayout header;
+    TextView tvActivePlayer, tvTimeLeft, tvRound;
+    int[] colors;
+    int[] clickCounter;
+    int headerBottomPos;
+    int activePlayer, roundNumber;
+    CountDownTimer timer;
+    Context context;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        context = this;
+
+        //Get data from intent
+        Intent intent = getIntent();
+        gridSize = intent.getIntExtra("GRID", 4);
+        players = intent.getIntExtra("PLAYERS", 1);
+        turnTime = intent.getIntExtra("TIME", 30);
+        rounds = intent.getIntExtra("ROUNDS", 3);
+
+        //RGB value of the colors: White, Light Grey, Dark Grey and Black
+        colors = new int[] {255,160,96,0};
+        //Keeps track of the number of clicks on each tile
+        clickCounter = new int[gridSize * gridSize];
+
+        createLayout();
+
+
+
+        timer = new CountDownTimer(turnTime*1000, 1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // Log.v("TAG", "seconds remaining: " + millisUntilFinished / 1000);
+                tvTimeLeft.setText(millisUntilFinished / 1000 + " Seconds");
+            }
+
+            @Override
+            public void onFinish() {
+                tvTimeLeft.setText("0 Seconds");
+                Toast.makeText(getApplicationContext(), "Times Up!!", Toast.LENGTH_SHORT).show();
+
+                activePlayer++;
+
+                if(activePlayer > players){
+                    activePlayer = 1;
+                    roundNumber ++;
+                }
+
+                if(roundNumber > rounds){
+                    //Finish the game
+                    Toast.makeText(getApplicationContext(), "Finished!", Toast.LENGTH_SHORT).show();
+                    AlertDialog finishDialog = new AlertDialog.Builder(context)
+                            .setMessage("The game is now finished!")
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    finish();
+                                }
+                            }).setCancelable(false).create();
+                    finishDialog.show();
+                    timer.cancel();
+                }else{
+                    //Dialog asking user to pass device
+                    AlertDialog dialog = new AlertDialog.Builder(context)
+                            .setMessage("Your turn has now finished. Please pass the device to Player " + activePlayer)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+
+                                    tvActivePlayer.setText("Player " + activePlayer);
+                                    if (activePlayer == 1){
+                                        Toast.makeText(getApplicationContext(), "Round " + roundNumber, Toast.LENGTH_SHORT).show();
+                                        tvRound.setText("Round " + roundNumber);
+                                    }
+                                    tvTimeLeft.setText(turnTime + " Seconds");
+                                    //Play
+                                    playGame();
+                                }
+                            }).setCancelable(false).create();
+                    dialog.show();
+                }
+            }
+        };
+
+        //First Player is player one
+        activePlayer = 1;
+        tvActivePlayer.setText("Player " + activePlayer);
+        //First Round is round one
+        roundNumber = 1;
+        tvRound.setText("Round " + roundNumber);
+        tvTimeLeft.setText(turnTime + " Seconds");
+        playGame();
+    }
+
+    private void playGame(){
+        //Ready dialog for next player
+        AlertDialog dialog2 = new AlertDialog.Builder(context)
+                .setMessage("Player " + activePlayer + "! Are you ready?")
+                .setPositiveButton("Let's Play!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        //Play
+                        timer.start();
+                    }
+                }).setCancelable(false).create();
+        dialog2.show();
+    }
+
+
+    private void createLayout() {
+        table = (TableLayout) findViewById(R.id.tableLayout);
+        root = (RelativeLayout) findViewById(R.id.root);
+        header = (RelativeLayout) findViewById(R.id.llHeader);
+        tvActivePlayer = (TextView) findViewById(R.id.tvActivePlayer);
+        tvTimeLeft = (TextView) findViewById(R.id.tvTimeLeft);
+        tvRound = (TextView) findViewById(R.id.tvRound);
+
+        header.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                header.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                drawMosaicView(table.getHeight(), table.getWidth());
+            }
+        });
+    }
+
+    private void drawMosaicView(int screenHeight, int screenWidth) {
+        Log.v("TAG", String.valueOf(screenWidth) + " width");
+        Log.v("TAG", String.valueOf(screenHeight) + " height");
+
+        //Individual height and width of the tiles
+        final int tileWidth = Math.round((float) screenWidth / gridSize);
+        final int tileHeight = Math.round((float) screenHeight / gridSize);
+
+        for(int i=0; i<gridSize; i++){
+            //Create TableRow as parent for this row
+            TableRow tableRow = new TableRow(this);
+
+            for(int j=0; j< gridSize; j++){
+                //Add tiles to the current row
+                final ImageView tile = new ImageView(this);
+                tile.setId(((gridSize*i) + j));
+                Log.v("TAG", String.valueOf(tileWidth) + " tile width");
+                Log.v("TAG", String.valueOf(tileHeight) + " tile height");
+                Bitmap bmp = Bitmap.createBitmap(tileWidth,tileHeight, Bitmap.Config.RGB_565);
+                //Set initial bitmap and color it white
+                tile.setImageBitmap(colorBitmap(bmp,0));
+                tile.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Bitmap bmp = ((BitmapDrawable)tile.getDrawable()).getBitmap();
+                        int id = v.getId();
+                        clickCounter[id]++;
+                        tile.setImageBitmap(colorBitmap(bmp,clickCounter[id]));
+                    }
+                });
+                tableRow.addView(tile);
+            }
+            table.addView(tableRow);
+        }
+
+        Log.v("TAG", String.valueOf(table.getMeasuredWidth()) + " table width");
+        Log.v("TAG", String.valueOf(table.getMeasuredHeight()) + " table height");
+    }
+
+    private Bitmap colorBitmap(Bitmap bmp, int clickNumber){
+        Rect r = new Rect(0, 0, bmp.getWidth(), bmp.getHeight());
+        Canvas canvas = new Canvas(bmp);
+        Paint paint = new Paint();
+        int colorNum = 0;
+
+        //Determine which shade of grey to use based on the number of clicks
+        if(clickNumber > colors.length-1){
+            colorNum = colors[clickNumber % colors.length];
+        }else{
+            colorNum = colors[clickNumber];
+        }
+
+        //Draw fill
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.rgb(colorNum,colorNum,colorNum));
+        canvas.drawRect(r, paint);
+
+        //Draw Border
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(4);
+        canvas.drawRect(r, paint);
+
+        return bmp;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        timer.cancel();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/uwais_000/mosaicgame/StartActivity.java
@@ -0,0 +1,134 @@
+package com.example.uwais_000.mosaicgame;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class StartActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener{
+
+    Spinner playersSpinner, gridSpinner, timeSpinner, roundsSpinner;
+    int[] playerArray, gridArray, timeArray, roundsArray;
+    int playerSelected, gridSelected, timeSelected, roundsSelected;
+    Button playButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        //Integer arrays of the equivalent data used in string arrays to populate spinners
+        playerArray = new int[] {1,2,3,4,5};
+        gridArray = new int[] {4,5,6,7,8,9,10};
+        timeArray = new int[] {30,45,60,90,120};
+        roundsArray = new int[]{3,5,7,10};
+
+        //Default values of the spinner selections - Do I really need these???
+        playerSelected = playerArray[0];
+        gridSelected = gridArray[0];
+        timeSelected = timeArray[0];
+        roundsSelected = roundsArray[0];
+
+        SetupSpinners();
+
+        playButton = (Button) findViewById(R.id.btnPlay);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //On Play Button Clicked
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("PLAYERS", playerSelected);
+                intent.putExtra("GRID", gridSelected);
+                intent.putExtra("TIME", timeSelected);
+                intent.putExtra("ROUNDS", roundsSelected);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void SetupSpinners() {
+        playersSpinner = (Spinner) findViewById(R.id.playersSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.players_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        playersSpinner.setAdapter(adapter);
+        playersSpinner.setOnItemSelectedListener(this);
+
+        gridSpinner = (Spinner) findViewById(R.id.gridSizeSpinner);
+        ArrayAdapter<CharSequence> gridAdapter = ArrayAdapter.createFromResource(this, R.array.gridSize_array, android.R.layout.simple_spinner_item);
+        gridAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gridSpinner.setAdapter(gridAdapter);
+        gridSpinner.setOnItemSelectedListener(this);
+
+        timeSpinner = (Spinner) findViewById(R.id.timeSpinner);
+        ArrayAdapter<CharSequence> timeAdapter = ArrayAdapter.createFromResource(this, R.array.turnTime_array, android.R.layout.simple_spinner_item);
+        timeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeSpinner.setAdapter(timeAdapter);
+        timeSpinner.setOnItemSelectedListener(this);
+
+        roundsSpinner = (Spinner) findViewById(R.id.roundsSpinner);
+        ArrayAdapter<CharSequence> roundsAdapter = ArrayAdapter.createFromResource(this, R.array.rounds_array, android.R.layout.simple_spinner_item);
+        roundsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        roundsSpinner.setAdapter(roundsAdapter);
+        roundsSpinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Spinner spinner = (Spinner) parent;
+        switch (spinner.getId()){
+            case R.id.playersSpinner:
+                playerSelected = playerArray[position];
+                break;
+
+            case R.id.gridSizeSpinner:
+                gridSelected = gridArray[position];
+                break;
+
+            case R.id.timeSpinner:
+                timeSelected = timeArray[position];
+                break;
+
+            case R.id.roundsSpinner:
+                roundsSelected = roundsArray[position];
+                Log.v("TAG", String.valueOf(roundsSelected));
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
